a637ef0382ab5cca1f56cf389c44bfa37ea4e658
nicklacno <nicholas.lacno@oit.edu>
2024-11-27 09:14:11 -0800

    Lab 6 Submission, can't get it to redirect though

---
+++ Lab6/Config/HeaderOptions.cs
@@ -0,0 +1,9 @@
+
+
+namespace Lab6;
+
+public class HeaderOptions
+{
+    public string LabNumber { get; set; }
+    public string AuthoredBy { get; set; }
+}
---
+++ Lab6/Middleware/OregonTechRedirectMiddleware.cs
@@ -0,0 +1,25 @@
+﻿namespace Lab6.Middleware
+{
+    public class OregonTechRedirectMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public OregonTechRedirectMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            context.Response.OnStarting(state => {
+                var context = (HttpContext)state;
+                context.Response.StatusCode = 302;
+                
+                return Task.CompletedTask;
+            }, context);
+
+            context.Response.Headers.Add("location", "https://www.oit.edu");
+        }
+    }
+}
+
